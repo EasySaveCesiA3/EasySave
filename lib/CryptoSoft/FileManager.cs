377f2a24511ca3020c69875f9c7941afdffea3fe
MartinWrob <martin.wroblewski@viacesi.fr>
2025-02-19 11:06:19 +0100

    Mise en place MVVM

---
+++ lib/CryptoSoft/FileManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace CryptoSoft
+{
+    public class FileManager
+    {
+        private readonly string FilePath;
+        private readonly byte[] Key;
+
+        public FileManager(string path, string key)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"Fichier introuvable : {path}");
+
+            if (string.IsNullOrEmpty(key) || Encoding.UTF8.GetByteCount(key) < 8)
+                throw new ArgumentException("La clé doit faire au moins 8 caractères.");
+
+            FilePath = path;
+            Key = Encoding.UTF8.GetBytes(key);
+        }
+
+        public int TransformFile()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            try
+            {
+                byte[] fileBytes = File.ReadAllBytes(FilePath);
+                byte[] transformedBytes = XorMethod(fileBytes, Key);
+                File.WriteAllBytes(FilePath, transformedBytes);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erreur : {ex.Message}");
+            }
+
+            stopwatch.Stop();
+            return (int)stopwatch.ElapsedMilliseconds;
+        }
+
+        private static byte[] XorMethod(byte[] fileBytes, byte[] keyBytes)
+        {
+            byte[] result = new byte[fileBytes.Length];
+
+            for (int i = 0; i < fileBytes.Length; i++)
+            {
+                result[i] = (byte)(fileBytes[i] ^ keyBytes[i % keyBytes.Length]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ lib/CryptoSoft/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CryptoSoft
+{
+    public static class CryptoSoftManager
+    {
+        public static void StartCrypto(string folderPath, List<string> selectedExtensions)
+        {
+            if (string.IsNullOrEmpty(folderPath) || !Directory.Exists(folderPath))
+                throw new DirectoryNotFoundException("Erreur : Dossier introuvable.");
+
+            var extensions = GetExtensionsFromFolder(folderPath);
+            if (extensions.Count == 0)
+                throw new Exception("Aucun fichier détecté dans ce dossier.");
+
+            if (selectedExtensions.Count == 0)
+                throw new Exception("Aucune extension sélectionnée pour le chiffrement.");
+
+            string key = ReadCryptageKey();
+            ProcessFiles(folderPath, key, selectedExtensions);
+        }
+
+        public static List<string> GetExtensionsFromFolder(string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+                throw new DirectoryNotFoundException("Le dossier spécifié n'existe pas.");
+
+            
+            List<string> extensions = Directory.GetFiles(folderPath)
+                .Select(file => Path.GetExtension(file).ToLower())
+                .Distinct()
+                .Where(ext => !string.IsNullOrEmpty(ext))
+                .OrderBy(ext => ext)
+                .ToList();
+
+            Console.WriteLine("Extensions trouvées : " + string.Join(", ", extensions));
+
+            return extensions;
+        }
+
+        public static void ProcessFiles(string folderPath, string key, List<string> selectedExtensions)
+        {
+            foreach (var file in Directory.GetFiles(folderPath))
+            {
+                if (!selectedExtensions.Contains(Path.GetExtension(file).ToLower()))
+                    continue;
+
+                try
+                {
+                    var fileManager = new FileManager(file, key);
+                    fileManager.TransformFile();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Erreur avec {file} : {ex.Message}");
+                }
+            }
+        }
+
+        public static string ReadCryptageKey()
+        {
+            return "MaSuperClé123"; // ⚠️ À sécuriser CHanger de procces pour demander de rentre la clé et pas de la mettre en statique
+        }
+    }
+}
