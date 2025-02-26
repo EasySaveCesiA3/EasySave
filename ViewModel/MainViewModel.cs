6c112d71988d020ff504cb19eab321981dc4e7b2
DeusErwan <erwan.hertz@viacesi.fr>
2025-02-26 16:41:23 +0100

    le delete

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿//using System.Windows;
+//using CommunityToolkit.Mvvm.ComponentModel;
+//using CommunityToolkit.Mvvm.Input;
+
+//namespace ViewModels
+//{
+//    public partial class MainViewModel : ObservableObject
+//    {
+//        [RelayCommand]
+//        private void DemarrerSauvegarde()
+//        {
+//            Views.SauvegardeWindow sauvegardeWindow = new Views.SauvegardeWindow();
+//            sauvegardeWindow.Show();
+//        }
+
+//        //[RelayCommand]
+//        //private void RestaurerSauvegarde()
+//        //{
+//        //    Views.RestoreWindow restoreWindow = new Views.RestoreWindow();
+//        //    restoreWindow.ShowDialog();
+//        //}
+
+//        //[RelayCommand]
+//        //private void ChangerLangue()
+//        //{
+//        //    Views.SelectionLangueWindow languageWindow = new Views.SelectionLangueWindow();
+//        //    languageWindow.ShowDialog();
+//        //}
+
+//        //[RelayCommand]
+//        //private void Quitter()
+//        //{
+//        //    Application.Current.Shutdown();
+//        //}
+//    }
+//}
